c028664a00e3483097f70b15e9c0e71a9fb2ec79
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-09 16:20:54 -0400

    array question solved for now & html page created to play around with document.write

--- arrays.js
+++ arrays.js
@@ -6,3 +6,7 @@
 // Create an array to hold your top choices (colors, presidents, whatever).
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is
+
+
+var topColors = ['red', 'blue', 'aubergine', 'cyan', 'azure'];
+document.write('My favorite color is ' + topColors[0] + '\nMy 2nd choice is ' + topColors[1] + '\n My 3rd choice is ' + topColors[2] + '\n My final favorite is ' + topColors[3]);
