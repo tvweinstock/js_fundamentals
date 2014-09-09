589af5f828c1189365df2298ceb8a5f5f5668fa2
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-09 19:29:42 -0400

    temp calculator created

--- variables.js
+++ variables.js
@@ -76,3 +76,11 @@
 // Convert it to fahrenheit and output "NN°C is NN°F".
 // Now store a fahrenheit temperature into a variable.
 // Convert it to celsius and output "NN°F is NN°C."
+
+
+var tempCel = 30
+var convertToFar = tempCel + '°C is ' + (tempCel * 9 / 5 + 32) + '°F';
+document.write(convertToFar)
+var tempFar = 74
+var convertToCel = tempFar + '°F is ' + ((tempFar - 32) * 5/9 ) + '°C';
+console.log(convertToCel);
