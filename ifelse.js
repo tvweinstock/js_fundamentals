7e1c8ced67869b520c18f023b50016541cb16f22
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-10 09:13:00 -0400

    noun pluralizer generated for ifelse.js

--- ifelse.js
+++ ifelse.js
@@ -22,10 +22,26 @@
 
 // EXERCISE: The World Translator
 
+
 // Write a function named helloWorld that:
 // takes 1 argument, a language code (e.g. "es", "de", "en")
 // returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
 // Call that function for each of the supported languages and log the result to make sure it works.
+// function helloWorld(language) {
+//   if (language === 'german') {
+//     return 'hallo welt';
+//   } else if (language === 'icelandic') {
+//     return 'halló heimur';
+//   } else if (language === 'french') {
+//     return 'bonjourrrrr';
+//     }  else {
+//       return 'hello World';
+//     }
+//   };
+
+// console.log(helloWorld('german'));
+// console.log(helloWorld('icelandic'));
+// console.log(helloWorld('french'));
 
 // EXERCISE: The Grade Assigner
 
@@ -33,6 +49,25 @@
 // takes 1 argument, a number score.
 // returns a grade for the score, either "A", "B", "C", "D", or "F".
 // Call that function for a few different scores and log the result to make sure it works.
+
+// function gradeAssigner(numScore) {
+//   if (numScore >= 90) {
+//     return 'A';
+//   } else if (numScore >= 80) {
+//     return 'B';
+//   } else if (numScore >= 70) {
+//     return 'C';
+//   } else if (numScore >= 60) {
+//     return 'D'; 
+//   } else {
+//     return 'F';
+//   }
+// };
+
+// console.log(gradeAssigner(77));
+// console.log(gradeAssigner(57));
+// console.log(gradeAssigner(7));
+// console.log(gradeAssigner(97));
 
 // EXERCISE: The Pluralizer
 
@@ -43,3 +78,18 @@
 // Bonus: Make it handle a few collective nouns like "sheep" and "geese".
 
 
+// function pluralizeThat(animal, number) {
+//   if (number > 1  && animal != 'sheep' && animal != 'geese' && animal != 'moose') {
+//     return number + ' ' + animal + 's';
+//   } else {
+//     return number + ' ' + animal;
+//   }
+// };
+
+// console.log(pluralizeThat('cat', 5));
+// console.log(pluralizeThat('dog', 5));
+// console.log(pluralizeThat('bird', 5));
+// console.log(pluralizeThat('geese', 5));
+// console.log(pluralizeThat('moose', 5));
+// console.log(pluralizeThat('sheep', 5));
+// console.log(pluralizeThat('dog', 1));
